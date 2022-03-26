0f9934299f568e6173123622e6194a1ecd4fab1e
Daniel Tang <tang.d@hotmail.com>
2022-03-26 19:42:14 +0800

    Change /ping endpoint.

--- project/tests/test_ping.py
+++ project/tests/test_ping.py
@@ -5,5 +5,5 @@ def test_ping(test_app):
     response = test_app.get("/ping")
     assert response.status_code == 200
 
-    expected_response = {"environment": "dev", "ping": "pong!", "testing": True}
+    expected_response = {"environment": "dev", "ping": "pong", "testing": True}
     assert response.json() == expected_response
